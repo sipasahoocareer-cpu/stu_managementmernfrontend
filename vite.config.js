fe6db6c2d38c1e06f3573198b0e55fa7e02f0bad
Rupa Sahoo <sipasahoo.career@gmail.com>
2026-07-06 12:56:07 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
